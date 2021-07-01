68460a96f5ba3a0a7b6e605c0dff21f77a90c15b
andy <andy.idiaghe@gmail.com>
2021-07-01 11:01:26 +0100

    Add Django Heroku db package and split requirements file

--- tokenizer/settings/prod.py
+++ tokenizer/settings/prod.py
@@ -1,6 +1,8 @@
 from tokenizer.settings.base import *
-
 import os
+import django_heroku
 
 DEBUG = os.environ.get('DEBUG_VALUE', False)
 ALLOWED_HOSTS = ['primer-token.herokuapp.com', '0.0.0.0']
+
+django_heroku.settings(locals())
